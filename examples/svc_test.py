840d5f12e123f82fc503cdf5da0d93b26b69f3d0
joyider <andre@sess.se>
2016-05-03 16:00:46 +0200

    sometimes you just herp the derp so hard it herpderps

--- examples/svc_test.py
+++ examples/svc_test.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3.5
 # -*- coding: utf-8 -*-
-# NoJoy_DI (c) 2016 by Andre Karlsson<andre.karlsson@protractus.se>
+# NoJoy-DI (c) 2016 by Andre Karlsson<andre.karlsson@protractus.se>
 #
-# NorseBot is licensed under a
-# Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+# This file is part of NoJoy_DI.
 #
-# You should have received a copy of the license along with this
-# work.  If not, see <http://creativecommons.org/licenses/by-nc-nd/3.0/>
+#    NoJoy_DI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    NoJoy_DI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 # Filename: svc_test.py by: andrek
@@ -14,21 +23,38 @@
 
 import unittest
 from NoJoy_DI.container import Container
+from NoJoy_DI.trees import DefaultTree, SingletonTree, BaseTree
+import pprint
 
 class MyParamService(object):
-    pass
+	pass
 
 class MyService(object):
-    def __init__(self, obj, text, value):
-        super(MyService, self).__init__()
-        print("service instance: %s" % obj)
-        print("container parameter: %s" % text)
-        print("provided value: %s" % value)
+	def __init__(self, obj, text, value):
+		super(MyService, self).__init__()
+		print("service instance: %s" % obj)
+		print("container parameter: %s" % text)
+		print("provided value: %s" % value)
+		print("I am: ", self.__class__)
 
 c = Container()
-
-c.add_service(MyParamService)
+mydict = {'className':'SomeClass',
+          'properties':[
+	          {'name':'response',
+                         'value':
+	                         {'type':'service','name':'response'}},
+                        {'name':'someFlag',
+                         'value':
+	                         {'type':'paramter','value':True}
+                         }
+                        ]
+          }
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(mydict)
+c.attempt(MyParamService)
 c.add_variables('Variable_name', "variable_data")
-c.add_service(MyService).types(obj__svc=MyParamService, text__param="Variable_name", value="The DATA")
+c.attempt(MyService, True).types(obj__svc=MyParamService, text__param="Variable_name", value="The DATA")
 
-c.get(MyService)
+id = c.get(MyService)
+id2 = c.getRaw(MyService)
+print(id2.tree)
